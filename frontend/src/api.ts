e8af0cf38fbca56b7bd031b3e86561a8641146ed
Izelude <olegm@gmx.de>
2026-05-03 21:53:41 +0200

    Revert "refactor frontend to be an actual ts react app"
    This reverts commit 66d3688493d0d19b3acd07e161c7856a8c10796e.

--- frontend/src/api.ts
+++
@@ -1,8 +0,0 @@
-export async function fetchJson<T = unknown>(url: string, init?: RequestInit): Promise<T> {
-    const response = await fetch(url, init);
-    const json = await response.json().catch(() => ({}));
-    if (!response.ok) {
-        throw new Error((json as any)?.error || `HTTP ${response.status}`);
-    }
-    return json as T;
-}
